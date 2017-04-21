b2ec2ef95ac217d1c13630b5ba17c7b5c90591ac
Shoili <shoili.pal@gmail.com>
2017-04-21 15:24:45 -0400

    text mining features

--- preprocess_shoili.R
+++ preprocess_shoili.R
@@ -35,8 +35,10 @@ library(RWeka)
 
 BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
 
-corp <- Corpus(VectorSource(listings$summary))
+# corp <- Corpus(VectorSource(listings$summary))
 # corp <- Corpus(VectorSource(listings$space))
+# corp <- Corpus(VectorSource(listings$description))
+corp <- Corpus(VectorSource(listings$transit))
 
 corp <- tm_map(corp, tolower)
 corp <- tm_map(corp, removePunctuation)
@@ -45,13 +47,31 @@ corp <- tm_map(corp, removeWords, stopwords("english"))
 
 tdm <- TermDocumentMatrix(corp, control = list(tokenize = BigramTokenizer))
 
-tdm <- removeSparseTerms(tdm, 0.99)
-print("----")
-print("tdm properties")
-str(tdm)
-tdm_top_N_percent = tdm$nrow / 100 * 20
+# tdm <- removeSparseTerms(tdm, 0.99)
+# print("----")
+# print("tdm properties")
+# str(tdm)
+# tdm_top_N_percent = tdm$nrow / 100 * 20
 
-inspect(tdm[1:20, 1:10])
-findFreqTerms(tdm, lowfreq = 300)
+# inspect(tdm[1:20, 1:10])
+findFreqTerms(tdm, lowfreq = 500)
 
-grepl("public transportation", tolower(listings$summary[14]))
+# publictransport <- as.numeric(grepl("transportation", tolower(listings$summary)))
+downtown <- as.numeric(grepl("downtown", tolower(listings$summary)) & grepl("downtown", tolower(listings$description)))
+kitchen <- as.numeric(grepl("kitchen", tolower(listings$summary)))
+restaurants <- as.numeric(grepl("restaurants", tolower(listings$summary)))
+laundry <- as.numeric(grepl("laundry", tolower(listings$description)))
+harvard <- as.numeric(grepl("harvard", tolower(listings$description)) & grepl("harvard", tolower(listings$summary)))
+bus <- as.numeric(grepl("bus", tolower(listings$transit)))
+subway <- as.numeric(grepl("subway", tolower(listings$transit)))
+airport <- as.numeric(grepl("airport", tolower(listings$transit)))
+nosmoking <- as.numeric(grepl("no smoking", tolower(listings$house_rules)))
+
+listings$space <- NULL
+listings$summary <- NULL
+listings$name <- NULL
+listings$description <- NULL
+listings$experiences_offered <- NULL
+listings$transit <- NULL
+listings$notes <- NULL
+listings$interaction <- NULL
