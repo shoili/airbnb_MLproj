2234838544317856661486f76eded60f259a7417
Shoili <shoili.pal@gmail.com>
2017-04-27 08:13:53 -0400

    different dataset

--- joins.R
+++ joins.R
@@ -1,8 +1,11 @@
 library(dplyr)
 
 available <- read.csv('available_listings.csv')
+available$X <- NULL
 listings <- read.csv("listings_final.csv")
+listings$X <- NULL
 reviews <- read.csv("Data/reviewsadj.csv")
+reviews$X <- NULL
 
 avail <- merge(listings, available, by.x="id", by.y="listing_id", all.y=TRUE)
 left <- merge(avail, reviews, by.x="id", by.y="listing_id", 
@@ -21,3 +24,19 @@ inner <- inner %>%
 
 #write.csv(left, "dataset1.csv")
 #write.csv(inner, "dataset2.csv")
+
+##################################################
+# de duplicated dataset without time series part
+
+
+available2 <- available %>%
+  group_by(listing_id) %>%
+  mutate(avgprice = mean(price_num),
+         pricevar = var(price_num)) %>%
+  select(listing_id, avgprice, pricevar)
+
+available2 <- unique(available2)
+
+available2_listings <- merge(listings, available2, by.x="id", by.y="listing_id", all.x = F, all.y = F)
+finaldata <- merge(available2_listings, reviews, by.x="id", by.y="listing_id")
+
